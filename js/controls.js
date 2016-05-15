72c6d9caf14ae0bad19a7053443c8a5419be32f9
Randy Tolentino <makertolentino@gmail.com>
2016-05-15 18:26:30 -0500

    play and pause working in js

--- js/controls.js
+++ js/controls.js
@@ -1,11 +1,16 @@
 var audioPlayer = document.getElementById('audioPlayer');
+var	playButton = document.getElementsByClassName('audio-button-play')[0];
+
+playButton.addEventListener('click', playOrPause, false);
 
 function playOrPause () {
 	if (!audioPlayer.paused && !audioPlayer.ended) {
 		audioPlayer.pause();
+		playButton.style.backgroundImage = 'url(../../../../assets/svg/play.svg)';
 	}
 	else {
 		audioPlayer.play();
+		playButton.style.backgroundImage = 'url(../../../../assets/svg/pause.svg)';
 	}
 }
 
