903fdf341374a5826c7b032bd928547d5bf7317e
Randy Tolentino <makertolentino@gmail.com>
2016-05-15 18:36:32 -0500

    mute and unmute working in js

--- js/controls.js
+++ js/controls.js
@@ -1,7 +1,9 @@
-var audioPlayer = document.getElementById('audioPlayer');
-var	playButton = document.getElementsByClassName('audio-button-play')[0];
+var audioPlayer = document.getElementById('audioPlayer'),
+	playButton = document.getElementById('playButton'),
+	muteButton = document.getElementById('muteButton');
 
 playButton.addEventListener('click', playOrPause, false);
+muteButton.addEventListener('click', muteOrUnmute, false);
 
 function playOrPause () {
 	if (!audioPlayer.paused && !audioPlayer.ended) {
@@ -14,3 +16,13 @@ function playOrPause () {
 	}
 }
 
+function muteOrUnmute () {
+	if (audioPlayer.muted == true) {
+		audioPlayer.muted = false;
+		muteButton.style.backgroundImage = 'url(../../../../assets/svg/sound.svg)';
+	}
+	else {
+		audioPlayer.muted = true;
+		muteButton.style.backgroundImage = 'url(../../../../assets/svg/mute.svg)';
+	}
+}
