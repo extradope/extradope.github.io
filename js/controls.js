6d7815ca6cedc19d6984b508d4ffa21d2a5f00c3
Randy Tolentino <makertolentino@gmail.com>
2016-05-15 19:00:13 -0500

    pausing js work, no pun

--- js/controls.js
+++ js/controls.js
@@ -1,6 +1,16 @@
 var audioPlayer = document.getElementById('audioPlayer'),
 	playButton = document.getElementById('playButton'),
-	muteButton = document.getElementById('muteButton');
+	muteButton = document.getElementById('muteButton'),
+	totalTime = document.getElementById('totalTime'),
+	currentTime = document.getElementById('currentTime');
+
+audioPlayer.addEventListener("loadedmetadata", changeDuration);
+
+function changeDuration() {
+	var minutes = parseInt(audioPlayer.duration/60);
+	var seconds = parseInt(audioPlayer.duration%60);
+	totalTime.innerHTML = minutes+":"+seconds;
+}
 
 playButton.addEventListener('click', playOrPause, false);
 muteButton.addEventListener('click', muteOrUnmute, false);
@@ -9,10 +19,12 @@ function playOrPause () {
 	if (!audioPlayer.paused && !audioPlayer.ended) {
 		audioPlayer.pause();
 		playButton.style.backgroundImage = 'url(../../../../assets/svg/play.svg)';
+		window.clearInterval(updateTime);
 	}
 	else {
 		audioPlayer.play();
 		playButton.style.backgroundImage = 'url(../../../../assets/svg/pause.svg)';
+		updateTime = setInterval(update, 500);
 	}
 }
 
@@ -24,5 +36,16 @@ function muteOrUnmute () {
 	else {
 		audioPlayer.muted = true;
 		muteButton.style.backgroundImage = 'url(../../../../assets/svg/mute.svg)';
+	}
+}
+
+function update() {
+	if (!audioPlayer.ended) {
+		var playedMinutes = parseInt(audioPlayer.currentTime/60);
+		var playedSeconds = parseInt(audioPlayer.currentTime%60);
+		currentTime.innerHTML = playedMinutes + ':' + playedSeconds;
+	}
+	else {
+		currentTime.innerHTML = '0.00';
 	}
 }
